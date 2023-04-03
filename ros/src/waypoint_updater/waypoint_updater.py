179a9342e05f1c33e0ba51784b43ab161038cfdf
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2023-04-03 18:09:05 +0100

    Add traffic waypoint subscriber

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,6 +3,7 @@
 import numpy as np
 import rospy
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Int32
 from styx_msgs.msg import Lane, Waypoint
 from scipy.spatial import KDTree
 
@@ -32,10 +33,10 @@ class WaypointUpdater(object):
 
         rospy.Subscriber("/current_pose", PoseStamped, self.pose_cb)
         rospy.Subscriber("/base_waypoints", Lane, self.waypoints_cb)
+        rospy.Subscriber("/traffic_waypoint", Int32, self.traffic_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-        # rospy.Subscriber("/traffic_waypoint", Waypoint, self.traffic_cb)
         # rospy.Subscriber("/obstacle_waypoint", Waypoint, self.obstacle_cb)
 
         self.final_waypoints_pub = rospy.Publisher(
@@ -113,7 +114,7 @@ class WaypointUpdater(object):
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        self.stopline_wp_idx = msg.data
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
